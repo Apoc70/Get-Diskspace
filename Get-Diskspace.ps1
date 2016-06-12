6ed3a5e97f99cc01308bf8e5280c729abf742217
Thomas Stensitzki <thomas@mcsmemail.de>
2016-06-12 14:34:40 +0200

    Get-Diskspace.ps1 updated

--- Get-Diskspace.ps1
+++ Get-Diskspace.ps1
@@ -7,7 +7,7 @@
     THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE  
     RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER. 
 
-    Version 1.11, 2016-05-11
+    Version 1.12, 2016-06-12
 
     Please send ideas, comments and suggestions to support@granikos.eu 
 
@@ -37,6 +37,7 @@
     1.0      Initial community release 
     1.1      Email reports added
     1.11     Send email issue fixed
+    1.12     PowerShell hygiene applied
 
     .PARAMETER ComputerName
     Can of the computer to fetch disk information from  
@@ -80,17 +81,17 @@ param(
 	[parameter(Mandatory=$false, HelpMessage='Computer name to query')]
 		[string] $ComputerName = $env:COMPUTERNAME,
     [parameter(Mandatory=$false, HelpMessage='Unit to convert to [default = GB]')]
-		[string] $Unit = "GB",
+		[string] $Unit = 'GB',
     [parameter(Mandatory=$false, HelpMessage='Fetch from all Exchange servers')]
         [switch] $AllExchangeServer,
     [parameter(Mandatory=$false, HelpMessage='Send report as Html email')]
         [switch] $SendMail,
     [parameter(Mandatory=$false, HelpMessage='Sender address for result summary')]
-        [string]$MailFrom = "",
+        [string]$MailFrom = '',
     [parameter(Mandatory=$false, HelpMessage='Recipient address for result summary')]
-        [string]$MailTo = "",
+        [string]$MailTo = '',
     [parameter(Mandatory=$false, HelpMessage='SMTP Server address for sending result summary')]
-        [string]$MailServer = ""
+        [string]$MailServer = ''
 )
 
 Set-StrictMode -Version Latest 
@@ -98,13 +99,13 @@ Set-StrictMode -Version Latest
 $Unit = $Unit.ToUpper()
 $now = Get-Date -Format F
 $ReportTitle = "Diskspace Report - $($now)"
-$global:Html = ""
+$global:Html = ''
 
 switch($Unit){
-    "GB" {
+    'GB' {
         $ConvertTo = 1GB
     }
-    "MB" {
+    'MB' {
         $ConvertTo = 1MB
     }
 }
@@ -114,13 +115,13 @@ param(
     [string] $ServerName
 )
     
-    if(($Unit -eq "GB") -or ($Unit -eq "MB")) {
+    if(($Unit -eq 'GB') -or ($Unit -eq 'MB')) {
 
         $ServerName = $ServerName.ToUpper()
 
         Write-Output "Fetching Volume Data from $($ServerName)"
 
-        $wmi = Get-WmiObject Win32_Volume -ComputerName $ServerName | Select Name, @{Label="Capacity ($Unit)";Expression={[decimal]::round($_.Capacity/$ConvertTo)}}, @{Label="FreeSpace ($Unit)";Expression={[decimal]::round($_.FreeSpace/$ConvertTo)}}, BootVolume, SystemVolume, FileSystem | Sort-Object Name 
+        $wmi = Get-WmiObject Win32_Volume -ComputerName $ServerName | Select-Object Name, @{Label="Capacity ($Unit)";Expression={[decimal]::round($_.Capacity/$ConvertTo)}}, @{Label="FreeSpace ($Unit)";Expression={[decimal]::round($_.FreeSpace/$ConvertTo)}}, BootVolume, SystemVolume, FileSystem | Sort-Object Name 
         $global:Html += $wmi | ConvertTo-Html -Fragment -PreContent "<h2>Server $($ServerName)</h2>"
     }
 
@@ -128,7 +129,7 @@ param(
 }
 
 Function Check-SendMail {
-     if( ($SendMail) -and ($MailFrom -ne "") -and ($MailTo -ne "") -and ($MailServer -ne "") ) {
+     if( ($SendMail) -and ($MailFrom -ne '') -and ($MailTo -ne '') -and ($MailServer -ne '') ) {
         return $true
      }
      else {
@@ -138,7 +139,7 @@ Function Check-SendMail {
 
 #### MAIN
 If (($SendMail) -and (!(Check-SendMail))) {
-    Throw "If -SendMail specified, -MailFrom, -MailTo and -MailServer must be specified as well!"
+    Throw 'If -SendMail specified, -MailFrom, -MailTo and -MailServer must be specified as well!'
 }
 
 # Some CSS to get a pretty report
@@ -188,16 +189,16 @@ if($AllExchangeServer) {
     $servers = Get-ExchangeServer | Sort-Object Name 
     foreach($server in $servers) {
         $output = Get-DiskspaceFromComputer -ServerName $server.Name 
-        if(!($SendMail)) { $output | FT -AutoSize } 
+        if(!($SendMail)) { $output | Format-Table -AutoSize } 
     }
 }
 else {
     $output = Get-DiskspaceFromComputer -ServerName $ComputerName
-    if(!($SendMail)) { $output | FT -AutoSize } 
+    if(!($SendMail)) { $output | Format-Table -AutoSize } 
 }
 
 if($SendMail) {
-    [string]$Body = ConvertTo-Html -Body $global:Html -Title "Status" -Head $head
+    [string]$Body = ConvertTo-Html -Body $global:Html -Title 'Status' -Head $head
     Send-MailMessage -From $MailFrom -To $Mailto -SmtpServer $MailServer -Body $Body -BodyAsHtml -Subject $ReportTitle
     Write-Output "Email sent to $($MailTo)"
 }
