20ab920b1e1ac39c5e871b4efef69a1db84c43b2
Thomas Stensitzki <thomas@mcsmemail.de>
2017-03-31 14:31:30 +0200

    1.1.3 Version number adjusted, minor PowerShell adjustments

--- Get-Diskspace.ps1
+++ Get-Diskspace.ps1
@@ -1,104 +1,98 @@
 <# 
-    .SYNOPSIS 
-    Fetches disk/volume information from a given computer
+  .SYNOPSIS 
+  Fetches disk/volume information from a given computer
 
-    Thomas Stensitzki 
+  Thomas Stensitzki 
 
-    THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE  
-    RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER. 
+  THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE  
+  RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER. 
 
-    Version 1.12, 2016-06-12
+  Version 1.1.3, 2017-03-31
 
-    Please send ideas, comments and suggestions to support@granikos.eu 
+  Please send ideas, comments and suggestions to support@granikos.eu 
 
-    .LINK 
-    More information can be found at http://www.granikos.eu/en/scripts
+  .LINK 
+  More information can be found at http://www.granikos.eu/en/scripts
 
-    .DESCRIPTION 
-    This script fetches disk/volume information from a given computer and displays 
+  .DESCRIPTION 
+  This script fetches disk/volume information from a given computer and displays 
 
-    * Volume name
-    * Capacity
-    * Free Space
-    * Boot Volume Status
-    * System Volume Status
-    * File Systemtype 
+  * Volume name
+  * Capacity
+  * Free Space
+  * Boot Volume Status
+  * System Volume Status
+  * File Systemtype 
 
-    With -SendMail switch no data is returned to the console. 
+  With -SendMail switch no data is returned to the console. 
      
-    .NOTES 
-    Requirements 
-    - Windows Server 2012 R2  
-    - Remote WMI
-    - Exchange Server Management Shell
+  .NOTES 
+  Requirements 
+  - Windows Server 2012 R2  
+  - Remote WMI
+  - Exchange Server Management Shell
     
-    Revision History 
-    -------------------------------------------------------------------------------- 
-    1.0      Initial community release 
-    1.1      Email reports added
-    1.11     Send email issue fixed
-    1.12     PowerShell hygiene applied
+  Revision History 
+  -------------------------------------------------------------------------------- 
+  1.0      Initial community release 
+  1.1      Email reports added
+  1.11     Send email issue fixed
+  1.12     PowerShell hygiene applied
+  1.1.3    Version number adjusted, minor PowerShell adjustments
 
-    .PARAMETER ComputerName
-    Can of the computer to fetch disk information from  
+  .PARAMETER ComputerName
+  Can of the computer to fetch disk information from  
 
-    .PARAMETER Unit
-    Target unit for disk space value (default = GB)
+  .PARAMETER Unit
+  Target unit for disk space value (default = GB)
 
-    .PARAMETER AllExchangeServer
-    Switch to fetch disk space data from all Exchange Servers
+  .PARAMETER AllExchangeServer
+  Switch to fetch disk space data from all Exchange Servers
 
-    .PARAMETER SendMail
-    Switch to send an Html report
+  .PARAMETER SendMail
+  Switch to send an Html report
 
-    .PARAMETER MailFrom
-    Email address of report sender
+  .PARAMETER MailFrom
+  Email address of report sender
 
-    .PARAMETER MailTo
-    Email address of report recipient
+  .PARAMETER MailTo
+  Email address of report recipient
 
-    .PARAMETER MailServer
-    SMTP Server for email report
+  .PARAMETER MailServer
+  SMTP Server for email report
 
-    .EXAMPLE 
-    Get disk information from computer MYSERVER
+  .EXAMPLE 
+  Get disk information from computer MYSERVER
     
-    Get-Diskpace.ps1 -ComputerName MYSERVER
+  Get-Diskpace.ps1 -ComputerName MYSERVER
 
-    .EXAMPLE
-    Get disk information from computer MYSERVER in MB
+  .EXAMPLE
+  Get disk information from computer MYSERVER in MB
 
-    Get-Diskpace.ps1 -ComputerName MYSERVER -Unit MB
+  Get-Diskpace.ps1 -ComputerName MYSERVER -Unit MB
 
-    .EXAMPLE
-    Get disk information from all Exchange servers and send html email
+  .EXAMPLE
+  Get disk information from all Exchange servers and send html email
 
-    Get-Diskpace.ps1 -AllExchangeServer -SendMail -MailFrom postmaster@sedna-inc.com -MailTo exchangeadmin@sedna-inc.com -MailServer mail.sedna-inc.com
+  Get-Diskpace.ps1 -AllExchangeServer -SendMail -MailFrom postmaster@sedna-inc.com -MailTo exchangeadmin@sedna-inc.com -MailServer mail.sedna-inc.com
 
 #>
 
+[CmdletBinding()]
 param(
-	[parameter(Mandatory=$false, HelpMessage='Computer name to query')]
-		[string] $ComputerName = $env:COMPUTERNAME,
-    [parameter(Mandatory=$false, HelpMessage='Unit to convert to [default = GB]')]
-		[string] $Unit = 'GB',
-    [parameter(Mandatory=$false, HelpMessage='Fetch from all Exchange servers')]
-        [switch] $AllExchangeServer,
-    [parameter(Mandatory=$false, HelpMessage='Send report as Html email')]
-        [switch] $SendMail,
-    [parameter(Mandatory=$false, HelpMessage='Sender address for result summary')]
-        [string]$MailFrom = '',
-    [parameter(Mandatory=$false, HelpMessage='Recipient address for result summary')]
-        [string]$MailTo = '',
-    [parameter(Mandatory=$false, HelpMessage='SMTP Server address for sending result summary')]
-        [string]$MailServer = ''
+		[string]$ComputerName = $env:COMPUTERNAME,
+		[string]$Unit = 'GB',
+    [switch]$AllExchangeServer,
+    [switch]$SendMail,
+    [string]$MailFrom = '',
+    [string]$MailTo = '',
+    [string]$MailServer = ''
 )
 
-Set-StrictMode -Version Latest 
 
 $Unit = $Unit.ToUpper()
 $now = Get-Date -Format F
-$ReportTitle = "Diskspace Report - $($now)"
+$ReportTitle = ('Diskspace Report - {0}' -f ($now))
 $global:Html = ''
 
 switch($Unit){
@@ -111,18 +105,19 @@ switch($Unit){
 }
 
 function Get-DiskspaceFromComputer {
-param(
+  [CmdletBinding()]
+  param(
     [string] $ServerName
-)
+  )
     
     if(($Unit -eq 'GB') -or ($Unit -eq 'MB')) {
 
         $ServerName = $ServerName.ToUpper()
 
-        Write-Output "Fetching Volume Data from $($ServerName)"
+        Write-Output ('Fetching Volume Data from {0}' -f ($ServerName))
 
         $wmi = Get-WmiObject Win32_Volume -ComputerName $ServerName | Select-Object Name, @{Label="Capacity ($Unit)";Expression={[decimal]::round($_.Capacity/$ConvertTo)}}, @{Label="FreeSpace ($Unit)";Expression={[decimal]::round($_.FreeSpace/$ConvertTo)}}, BootVolume, SystemVolume, FileSystem | Sort-Object Name 
-        $global:Html += $wmi | ConvertTo-Html -Fragment -PreContent "<h2>Server $($ServerName)</h2>"
+        $global:Html += $wmi | ConvertTo-Html -Fragment -PreContent ('<h2>Server {0}</h2>' -f ($ServerName))
     }
 
     $wmi
@@ -143,11 +138,12 @@ If (($SendMail) -and (!(Check-SendMail))) {
 }
 
 # Some CSS to get a pretty report
-$head = @'
+$head = @"
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html><head><title>$($ReportTitle)</title>
-<style type=”text/css”>
-<!–
+<style type="text/css">
+<!--
 body {
     font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
 }
@@ -181,24 +177,34 @@ table th {
     padding-right: 20px;
     text-align: left;
 }
-->
+-->
 </style>
-'@
+"@
 
 if($AllExchangeServer) {
     $servers = Get-ExchangeServer | Sort-Object Name 
+
     foreach($server in $servers) {
+
         $output = Get-DiskspaceFromComputer -ServerName $server.Name 
+
         if(!($SendMail)) { $output | Format-Table -AutoSize } 
+
     }
 }
 else {
+
     $output = Get-DiskspaceFromComputer -ServerName $ComputerName
+
     if(!($SendMail)) { $output | Format-Table -AutoSize } 
+
 }
 
 if($SendMail) {
+
     [string]$Body = ConvertTo-Html -Body $global:Html -Title 'Status' -Head $head
+    
     Send-MailMessage -From $MailFrom -To $Mailto -SmtpServer $MailServer -Body $Body -BodyAsHtml -Subject $ReportTitle
-    Write-Output "Email sent to $($MailTo)"
+    
+    Write-Output ('Email sent to {0}' -f ($MailTo))
 }
